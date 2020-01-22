d41823823e285436ffeed900c9ff12513ae5556d
DESKTOP-김진영\rlawl <rlawlsdud419@naver.com>
2020-01-22 14:55:49 +0900

    fail on unknown properties

--- src/test/java/me/kjy/restapitest/events/EventControllerTests.java
+++ src/test/java/me/kjy/restapitest/events/EventControllerTests.java
@@ -52,8 +52,8 @@ public class EventControllerTests {
 
     @Test
     public void createEvent() throws Exception {
-        Event event=Event.builder()
-                .id(100)
+        // 입력값 검사
+        EventDto event=EventDto.builder()
                 .name("Spring")
                 .description("Rest API Development with Spring")
                 .beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23,14,21))
@@ -64,9 +64,6 @@ public class EventControllerTests {
                 .maxPrice(200)
                 .limitOfEnrollment(100)
                 .location("강남역 D2 스타텁 팩토리")
-                .free(true)
-                .offline(false)
-                .eventStatus(EventStatus.PUBLISHED)
                 .build();
 
         // Mock 객체이므로 save를 하더라도 리턴되는 값이 모두 null이므로 Mockito.when을 통해
@@ -87,6 +84,31 @@ public class EventControllerTests {
         ;
     }
 
+    @Test
+    public void createEvent_Bad_request() throws Exception { //필요 이상 입력값 입력시 400오류
+        Event event = Event.builder()
+                .id(100)
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,11,23,14,22))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,11,24,14,22))
+                .beginEventDateTime(LocalDateTime.of(2018,11,25,14,22))
+                .endEventDateTime(LocalDateTime.of(2018,11,26,14,22))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .free(true)
+                .offline(false)
+                .eventStatus(EventStatus.PUBLISHED)
+                .build();
 
-
+        mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+        ;
+    }
 }
