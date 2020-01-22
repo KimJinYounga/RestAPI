cc098e572cd3f5ff8106cc35340518e03702dad5
DESKTOP-김진영\rlawl <rlawlsdud419@naver.com>
2020-01-22 17:27:51 +0900

    Business logic tests

--- src/main/java/me/kjy/restapitest/events/Event.java
+++ src/main/java/me/kjy/restapitest/events/Event.java
@@ -33,5 +33,22 @@ public class Event {
     private EventStatus eventStatus = EventStatus.DRAFT ;
 
 
+    public void update() {
+        // Update free
+        if(this.basePrice == 0 && this.maxPrice == 0) {
+            this.free=true;
+        }
+        else {
+            this.free=false;
+        }
+
+        // Update offline
+        if(this.location == null) {
+            this.offline = false;
+        }
+        else {
+            this.offline = true;
+        }
+    }
 }
 
